cacd93631f109785b36a325e41794001af1e4ed0
zirtaeb-alb <88463346+zirtaeb-alb@users.noreply.github.com>
2021-08-29 23:48:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ mongoose.connect('mongodb+srv://beatriz:jrODV0NOkUwAPjLn@cluster0.m3cil.mongodb.
     useNewUrlParser: true,
 });
 
+app.get("/", function(req, res) {
+    res.sendFile( __dirname + "./frontend/index.html");
+});
 
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
