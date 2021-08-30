361c17d54e66ffc39cc160cd5b477b7d61903fea
zirtaeb-alb <88463346+zirtaeb-alb@users.noreply.github.com>
2021-08-29 21:37:12 -0300

    Update index.js
    atualizado index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
+
+
 
 mongoose.connect('mongodb+srv://beatriz:jrODV0NOkUwAPjLn@cluster0.m3cil.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
